acb6f2bc82dfa7eb438f461da2385c111999a0c3
elisee-assinou <pascal.assinou@epitech.eu>
2023-12-05 08:44:10 +0100

    beta verse

--- dcm_apk/screens/SignupScreen/index.js
+++ dcm_apk/screens/SignupScreen/index.js
@@ -1,20 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert, Image, Picker } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    Alert,
+    Image,
+    Modal,
+    FlatList,
+    TouchableWithoutFeedback,
+} from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { API_URL } from '../../utils/constantes';
 
-const SignUpScreen = () => {
-    const navigation = useNavigation();
+const SignUpScreen = ({ navigation }) => {
+    const [name, setName] = useState('');
+    const [lastname, setLastname] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
     const [address, setAddress] = useState('');
     const [selectedAgency, setSelectedAgency] = useState('');
-    const [agencies, setAgencies] = useState([]); // Array to store agencies fetched from backend
-
-    
+    const [agencies, setAgencies] = useState([]);
+    const [isAgencyModalVisible, setIsAgencyModalVisible] = useState(false);
 
     // Fetch the list of agencies when the component mounts
     useEffect(() => {
@@ -23,11 +35,11 @@ const SignUpScreen = () => {
 
     const fetchAgencies = async () => {
         try {
-            const url=`${API_URL}/login`
+            const url = `${API_URL}/agency`;
             const response = await fetch(url);
             if (response.ok) {
                 const data = await response.json();
-                setAgencies(data); 
+                setAgencies(data);
             } else {
                 console.error('Error fetching agencies:', response.status);
             }
@@ -43,14 +55,17 @@ const SignUpScreen = () => {
         }
 
         try {
+            const url = `${API_URL}/signup`;
             const formData = new FormData();
+            formData.append('name', name);
+            formData.append('lastname', lastname);
             formData.append('email', email);
             formData.append('password', password);
             formData.append('phoneNumber', phoneNumber);
             formData.append('address', address);
             formData.append('selectedAgency', selectedAgency);
 
-            const response = await fetch('http://your-backend-url/signup', {
+            const response = await fetch(url, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'multipart/form-data',
@@ -59,6 +74,8 @@ const SignUpScreen = () => {
             });
 
             if (response.ok) {
+                const responseData = await response.json();
+                await AsyncStorage.setItem('token', responseData.token);
                 navigation.navigate('Login');
             } else {
                 const errorData = await response.json();
@@ -67,15 +84,52 @@ const SignUpScreen = () => {
         } catch (error) {
             console.error('Erreur lors de l\'inscription :', error.message);
         }
+
+        setIsAgencyModalVisible(false);
     };
+
+    const renderAgencyItem = ({ item }) => (
+        <TouchableOpacity
+          style={styles.agencyItem}
+          onPress={() => {
+            setSelectedAgency(item.id);
+            setIsAgencyModalVisible(false);
+          }}
+        >
+          <Text>{item.name}</Text>
+        </TouchableOpacity>
+      );
+      
+
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>SIGNUP</Text>
+            <Text style={styles.title}>SIGN UP</Text>
 
-            <Image style={styles.image} source={require('../../assets/im1.jpeg')} />
 
-            {/* Champ de saisie pour l'email avec icône */}
+            {/* name*/}
+            <View style={styles.inputContainer}>
+                <FontAwesome5 name="user" size={20} color="#333" style={styles.icon} />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Nom"
+                    value={name}
+                    onChangeText={(text) => setName(text)}
+                />
+            </View>
+
+            {/* lastname*/}
+            <View style={styles.inputContainer}>
+                <FontAwesome5 name="user" size={20} color="#333" style={styles.icon} />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Prenoms"
+                    value={lastname}
+                    onChangeText={(text) => setLastname(text)}
+                />
+            </View>
+
+            {/* email*/}
             <View style={styles.inputContainer}>
                 <FontAwesome5 name="envelope" size={20} color="#333" style={styles.icon} />
                 <TextInput
@@ -88,7 +142,7 @@ const SignUpScreen = () => {
                 />
             </View>
 
-            {/* Champ de saisie pour le mot de passe avec icône */}
+            {/* password*/}
             <View style={styles.inputContainer}>
                 <FontAwesome5 name="lock" size={20} color="#333" style={styles.icon} />
                 <TextInput
@@ -100,7 +154,7 @@ const SignUpScreen = () => {
                 />
             </View>
 
-            {/* Champ de saisie pour la confirmation du mot de passe avec icône */}
+            {/* confirm password */}
             <View style={styles.inputContainer}>
                 <FontAwesome5 name="lock" size={20} color="#333" style={styles.icon} />
                 <TextInput
@@ -112,7 +166,7 @@ const SignUpScreen = () => {
                 />
             </View>
 
-            {/* Champ de saisie pour le numéro de téléphone avec icône */}
+            {/* telephone */}
             <View style={styles.inputContainer}>
                 <FontAwesome5 name="phone" size={20} color="#333" style={styles.icon} />
                 <TextInput
@@ -124,7 +178,7 @@ const SignUpScreen = () => {
                 />
             </View>
 
-            {/* Champ de saisie pour l'adresse avec icône */}
+            {/* adresse */}
             <View style={styles.inputContainer}>
                 <FontAwesome5 name="map-marker-alt" size={20} color="#333" style={styles.icon} />
                 <TextInput
@@ -135,20 +189,37 @@ const SignUpScreen = () => {
                 />
             </View>
 
-            {/* Dropdown/select field for agency selection */}
-            <View style={styles.inputContainer}>
+
+            {/* Agency selection */}
+            <TouchableOpacity
+                style={styles.inputContainer}
+                onPress={() => setIsAgencyModalVisible(true)}
+            >
                 <FontAwesome5 name="building" size={20} color="#333" style={styles.icon} />
-                <Picker
-                    style={styles.input}
-                    selectedValue={selectedAgency}
-                    onValueChange={(itemValue) => setSelectedAgency(itemValue)}
-                >
-                    <Picker.Item label="Select Agency" value="" />
-                    {agencies.map((agency) => (
-                        <Picker.Item key={agency.id} label={agency.name} value={agency.id.toString()} />
-                    ))}
-                </Picker>
-            </View>
+                <Text style={styles.selectedAgencyText}>
+                    {selectedAgency ? agencies.find((a) => a.id === selectedAgency)?.name : 'Sélectionner une agence'}
+                </Text>
+            </TouchableOpacity>
+
+            {/* Agency selection modal */}
+            <Modal
+                transparent
+                animationType="slide"
+                visible={isAgencyModalVisible}
+                onRequestClose={() => setIsAgencyModalVisible(false)}
+            >
+                <TouchableWithoutFeedback onPress={() => setIsAgencyModalVisible(false)}>
+                    <View style={styles.modalOverlay} />
+                </TouchableWithoutFeedback>
+                <View style={styles.modalContent}>
+                    <FlatList
+                        data={agencies}
+                        keyExtractor={(item) => (item && item.id ? item.id.toString() : Math.random().toString())}
+                        renderItem={renderAgencyItem}
+                    />
+
+                </View>
+            </Modal>
 
             <TouchableOpacity style={styles.button} onPress={handleSignUp}>
                 <Text style={styles.buttonText}>S'INSCRIRE</Text>
@@ -181,29 +252,30 @@ const styles = StyleSheet.create({
     inputContainer: {
         flexDirection: 'row',
         alignItems: 'center',
-        width: '80%',
+        width: '95%',
         marginBottom: 16,
         borderBottomWidth: 2,
         borderBottomColor: '#333',
         borderRadius: 5,
         backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        paddingVertical: 12,
     },
     icon: {
-        marginRight: 8,
-        marginLeft: 8,
+        marginHorizontal: 8, // Adjusted margin for a more balanced look
     },
     input: {
         flex: 1,
         height: 40,
         paddingHorizontal: 8,
         color: '#333',
+        fontSize: 16, // Adjusted font size
     },
     button: {
         backgroundColor: '#000',
         paddingVertical: 12,
         paddingHorizontal: 24,
         borderRadius: 5,
-        margin: 16,
+        marginVertical: 16, // Adjusted vertical margin
     },
     buttonText: {
         color: '#fff',
@@ -213,6 +285,32 @@ const styles = StyleSheet.create({
         marginTop: 16,
         color: '#fff',
         fontSize: 20,
+    },
+
+    selectedAgencyText: {
+        flex: 1,
+        paddingHorizontal: 8,
+        color: '#333',
+    },
+    modalOverlay: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        paddingVertical: 16,
+        paddingHorizontal: 16,
+    },
+    agencyItem: {
+        paddingVertical: 12,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
     },
 
 });
--- dcm_apk/utils/constantes.js
+++ dcm_apk/utils/constantes.js
@@ -1,1 +1,1 @@
-export const API_URL="http://10.50.5.9:5010/api/v1"
+export const API_URL="http://172.20.10.3:5010/api/v1"
