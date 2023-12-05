bcc0f1b09a5983dd28f9a70e09e5963f40d33176
stilisky <setonde.hounnouvi@epitech.eu>
2023-12-05 08:55:48 +0100

    Sale route added

---
+++ dcm_api/routes/saleRoute.js
@@ -0,0 +1,16 @@
+const express = require('express')
+var router = express.Router()
+const {authAdmin, auth} = require('../middlewares/authMiddleware')
+const {
+   getSales,
+   getSale,
+   createSale,
+   updateSale,
+   deleteSale
+} = require('../controllers/saleController')
+
+router.route('/').get(authAdmin, getSales)
+router.route('/saleId').get(auth, getSale).put(auth, updateSale).delete(authAdmin, deleteSale)
+router.route('/client/clientId').post(createSale)
+
+module.exports = router
