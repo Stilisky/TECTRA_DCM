9ca1334548db14d6c4e918b08ed414f14f6cccfc
stilisky <setonde.hounnouvi@epitech.eu>
2023-12-06 21:10:39 +0100

    User service update

--- dcm_api/Services/userServices.js
+++ dcm_api/Services/userServices.js
@@ -1,12 +1,12 @@
 const userModel = require('../models/User')
 
 const getUsers = async () => {
-   const users = await userModel.find({}, '-password').populate('agency')
+   const users = await userModel.find({}, '-password').populate('ventes')
    return users
 }
 
 const getUser = async (id) => {
-   const user = await userModel.findById(id, '-password').populate('agency')
+   const user = await userModel.findById(id, '-password').populate('ventes')
    return user;
 }
 
