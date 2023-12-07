6d651162f4569e32c32b8433529072419850f2ce
stilisky <setonde.hounnouvi@epitech.eu>
2023-12-07 14:29:13 +0100

    Profil updated

--- dcm_apk/src/screens/App/ProfilScreen/index.js
+++ dcm_apk/src/screens/App/ProfilScreen/index.js
@@ -1,12 +1,12 @@
 // ProfilScreen.js
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, Button, SafeAreaView, ScrollView, StatusBar, Image } from 'react-native';
+import { View, Text, StyleSheet, Button, SafeAreaView, ScrollView, StatusBar, Image, TouchableOpacity } from 'react-native';
 import { UserContext } from '../../../context/userContext';
+import { SCREEN } from '../../../components/utils/constantes';
+import { fonts, sizes } from '../../../components/utils/theme';
 
 const ProfilScreen = ({ navigation, route }) => {
-  const {user, logout} = useContext(UserContext)
-
- 
+  const {user, logout} = useContext(UserContext) 
 
   const handleLogout = async () => {
     logout()
@@ -15,7 +15,36 @@ const ProfilScreen = ({ navigation, route }) => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-          {/* <Image source={}/> */}
+        <View>
+          <Image source={require(`../../../assets/user.png`)} resizeMode='contain' style={{ width: (SCREEN.width -100), height: 300, paddingHorizontal:50 }}/>
+        </View>
+        <View style={{ justifyContent: 'flex-start', marginLeft:15 }}>
+
+          <View style={{ flexDirection:'row' }}>
+            <Text style={{ fontFamily: fonts.font600, fontSize: sizes.xl, color:'#4F95DC' }}>Nom:</Text>
+            <Text style={{ fontSize: sizes.xl, paddingLeft: 5 }}>{user.name}</Text>
+          </View>
+
+          <View style={{ flexDirection:'row' }}>
+            <Text style={{ fontFamily: fonts.font600, fontSize: sizes.xl, color:'#4F95DC' }}>Prénoms:</Text>
+            <Text style={{ fontSize: sizes.xl, paddingLeft: 5 }}>{user.lastName}</Text>
+          </View>
+
+          <View style={{ flexDirection:'row' }}>
+            <Text style={{ fontFamily: fonts.font600, fontSize: sizes.xl, color:'#4F95DC' }}>Email:</Text>
+            <Text style={{ fontSize: sizes.xl, paddingLeft: 5 }}>{user.email}</Text>
+          </View>
+
+          <View style={{ flexDirection:'row', justifyContent: 'center', marginTop: 5 }}>
+            <TouchableOpacity
+            onPress={handleLogout}
+              style={{ backgroundColor:'#4F95DC', padding: 10,paddingHorizontal:20, borderRadius: 20 }}
+            >
+              <Text style={{ color:'white' }}>Deconnexion</Text>
+            </TouchableOpacity>
+          </View>
+
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -27,6 +56,7 @@ const styles = StyleSheet.create({
     marginTop: StatusBar.currentHeight,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'white'
   },
   title: {
     fontSize: 24,
