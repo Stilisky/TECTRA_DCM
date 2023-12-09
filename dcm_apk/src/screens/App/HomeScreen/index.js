5fd498febdca5905a1bd06d3a5d6ad2421466e4a
elisee-assinou <pascal.assinou@epitech.eu>
2023-12-09 11:34:36 +0100

    beta version

--- dcm_apk/src/screens/App/HomeScreen/index.js
+++ dcm_apk/src/screens/App/HomeScreen/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image, TouchableOpacity, FlatList, StatusBar } from 'react-native';
+import { StyleSheet, View, Text, Image, TouchableOpacity, FlatList, StatusBar,SafeAreaView } from 'react-native';
 import { fonts, sizes } from '../../../components/utils/theme';
 
 export default function HomeScreen() {
   const products = [
-    { id: '1', title: 'SAC DE RIZ ', price: '25000FCFA ', image: require('../../../assets/sacderiz.avif') },
-    { id: '2', title: ' CEREALES', price: '650FCFA', image: require('../../../assets/cereales.avif') },
-    { id: '3', title: ' Riz parfume', price: '1000 FCFA', image: require('../../../assets/rizcuit.avif') },
+    { id: '1', title: 'FER A BETON  ', price: '25000FCFA ', image: require('../../../assets/fab.jpeg') },
+    { id: '2', title: ' SERRURE', price: '650FCFA', image: require('../../../assets/serrure.jpeg') },
+    { id: '3', title: ' SACHET DE VIS', price: '1000 FCFA', image: require('../../../assets/vis.jpeg') },
 
 
     { id: '4', title: 'TULE ONDULEE', price: '5500FCFA', image: require('../../../assets/tule.avif') },
     { id: '5', title: 'TUYAU PVC', price: '12000 FCFA ', image: require('../../../assets/tuyau.avif') },
-    { id: '6', title: 'SACS DAMME', price: '35000FCFA', image: require('../../../assets/1.avif') },
+    { id: '6', title: ' FER A BETON', price: '2500FCFA', image: require('../../../assets/fer.jpeg') },
     // Ajoutez autant d'éléments que nécessaire
   ];
 
@@ -30,7 +30,7 @@ export default function HomeScreen() {
   );
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Liste des Produits</Text>
       <FlatList
         data={products}
@@ -39,7 +39,7 @@ export default function HomeScreen() {
         renderItem={renderProductItem}
         contentContainerStyle={styles.productsList}
       />
-    </View>
+    </SafeAreaView>
   );
 }
 
